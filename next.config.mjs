6e42fd93dc2e59f59b1d53d480e6f4eca45039e1
mateuszkwasniak <mz.kwasniak@gmail.com>
2024-03-15 15:30:22 +0100

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+//ta konfiguracja umozliwia hot-reload przy froncie odpalonym z Dockera...
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
+};
 
 export default nextConfig;
